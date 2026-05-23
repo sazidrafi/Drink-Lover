94576b12949629fac0ef64028ac6468fb18d5a2d
sazidul <sazidrafi1234@gmail.com>
2026-05-23 20:16:58 +0600

    feat: implement search by name and fix details button handler

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-//add eventlistener to button
-document.getElementById("search-button").addEventListener("click", (event) => {
-    const inputValue = document.getElementById("input-field").value;
-    console.log("Input value: ", inputValue);
-    //food show koro
-    ShowFoods(inputValue);
-    document.getElementById("input-field").value = ""
-})
-//get input from from keyboard pressing "enter" 
-document.addEventListener("keydown", (event) => {
-    const inputValue = document.getElementById("input-field").value;
-    if (event.key === "Enter") {
-        console.log("Input value: ", inputValue);
-        ShowFoods(inputValue);
-        document.getElementById("input-field").value = ""
-    }
 
-})
 //input field er kaj ses
 
 
@@ -59,7 +42,9 @@ const ShowFoodsDefault = () => {
                     <button class="add-to-cart-btn">Add to cart</button>
                     <button class="details-btn">Details</button>
                     `
-                        div.addEventListener("click", (event) => {
+                        //details button e click korle modal er moddhe detils show koro
+                        const selector = div.querySelector(".details-btn")
+                        selector.addEventListener("click", (event) => {
                             //each card e click korle modal pop up hbe, then details show korbe
                             ShowDetailsThroughModal(element)
                         })
@@ -75,14 +60,70 @@ const ShowFoodsDefault = () => {
 ShowFoodsDefault()
 
 
+
+//add eventlistener to button
+document.getElementById("search-button").addEventListener("click", (event) => {
+    const inputValue = document.getElementById("input-field").value;
+    console.log("Input value: ", inputValue);
+    //food show koro
+    ShowSearchedFood(inputValue);
+    document.getElementById("input-field").value = ""
+})
+//get input from from keyboard pressing "enter" 
+document.addEventListener("keydown", (event) => {
+    const inputValue = document.getElementById("input-field").value;
+    if (event.key === "Enter") {
+        console.log("Input value: ", inputValue);
+        ShowFoods(inputValue);
+        document.getElementById("input-field").value = ""
+    }
+
+})
+
+
+
+//modal er moddhe value show korar jnno method
+const ShowDetailsThroughModal = (element) => {
+    //ekhane modal add koro
+}
+
 //seach er basis e food show koro
-const ShowFoods = (value) => {
+const ShowSearchedFood = (value) => {
     //value fetch kore ene show korte hbe
-    const foodContainer = document.getElementById
-        ("show-all-items")
-    const div = document.createElement("div")
-    div.innerHTML = `
-    <p>You searched for: ${value}
-    `
-    foodContainer.appendChild(div)
+document.getElementById("show-all-items").innerHTML = "";
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
+        .then(response => response.json())
+        .then(elements => {
+            console.log(elements);
+            if (elements.drinks) {
+                    for (let element of elements.drinks) {
+                        //each element accessed!
+                        //akhn show koro
+                        const container = document.getElementById("show-all-items")
+                        // container.innerHTML="";
+                        const div = document.createElement("div")
+                        div.classList.add("card-design")
+                        div.id = "card-design"
+                        div.innerHTML = `
+                    <img src="${element.strDrinkThumb}" class="img-size" alt="">
+                    <p><span class="name-tag">Name: ${element.strDrink}</span><span><br></span>Category: ${element.strCategory}</p>
+                    <p class="instruction">Instruction: ${element.strInstructions}</p>
+                    <button class="add-to-cart-btn">Add to cart</button>
+                    <button class="details-btn">Details</button>
+                    `
+                        //details button e click korle modal er moddhe detils show koro
+                        const selector = div.querySelector(".details-btn")
+                        selector.addEventListener("click", (event) => {
+                            //each card e click korle modal pop up hbe, then details show korbe
+                            ShowDetailsThroughModal(element)
+                        })
+                        container.appendChild(div)
+                    }
+                }
+
+                else{
+                    //show searched drink not available
+                    alert("Item not found")
+                }
+        })
 }
